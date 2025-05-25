049cca2a26b184321c12da0d38f91c7c153d52ec
takeru.shingu <takeru.shingu@town.biz>
2025-05-25 11:22:43 +0900

    disable prepared statements for Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+// src/lib/prisma.ts
+
+import { PrismaClient } from '@/generated/prisma'; // ← ここを修正
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-    // ✅ Prepared statements を無効化
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL + '&pgbouncer=true', // 明示的に追記しておく
-      },
-    },
-  });
+  globalForPrisma.prisma ?? new PrismaClient({ log: ['query'] });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
