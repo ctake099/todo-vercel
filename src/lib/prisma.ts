35179151f1dda724fd6c2005dbf6017b2707c735
takeru.shingu <takeru.shingu@town.biz>
2025-05-25 11:02:01 +0900

    prisma.tsの変更

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,11 @@
-// src/lib/prisma.ts
-import { PrismaClient } from '@/generated/prisma'
+import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+  log: ['query'],
+  // ここで prepared statements を無効化
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+})
